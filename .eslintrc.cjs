9026420cf92458f8bf141920dd18ed85ba7083ee
TigranAvushyan <tigran.avushyan@gmail.com>
2024-07-28 12:26:28 +0300

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:prettier/recommended',
+
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'check-file'],
+	rules: {
+		'prettier/prettier': 'error',
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'check-file/filename-naming-convention': [
+			'error',
+			{ '**/*.{js,ts,tsx,scss}': 'KEBAB_CASE' },
+			{ ignoreMiddleExtensions: true },
+		],
+	},
+}
